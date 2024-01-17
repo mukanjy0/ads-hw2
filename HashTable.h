ec04f95ccdb4e3d3a4341ab832fae8ca2306640f
Mukanjyo <122047216+mukanjy0@users.noreply.github.com>
2024-01-17 12:45:20 -0500

    added singly linked list implementation & hash table template

--- HashTable.h
+++ HashTable.h
@@ -5,4 +5,83 @@
 #ifndef ADS_HW2_HASHTABLE_H
 #define ADS_HW2_HASHTABLE_H
 
+#include <vector>
+using namespace std;
+
+typedef long long ll;
+
+template<typename T, typename U>
+class HashTable {
+    ll a{};
+    int r{};
+    int sz{};
+    int upper{};
+    int lower{};
+
+    int _hash(int k, int m) {
+
+    }
+
+    void _compute_bounds() {
+
+    }
+
+    void _resize(int n) {
+
+    }
+
+public:
+    HashTable(int r = 200) : r(r) {}
+
+    HashTable(const vector<T> &other, int r = 200) : r(r) {}
+
+    ~HashTable() {}
+
+    int size() { return sz; }
+
+    struct iterator {
+
+    };
+
+    void build(const vector<T> &other) {
+
+    }
+
+    bool find(int k) {
+
+    }
+
+    bool insert(const pair<T, U> &x) {
+
+    }
+
+    pair<T, U> erase(int k) {
+
+    }
+
+    iterator find_min() {
+
+    }
+
+    iterator find_max() {
+
+    }
+
+    iterator find_next() {
+
+    }
+
+    iterator find_prev() {
+
+    }
+
+    iterator begin() {
+
+    }
+
+    iterator end() {
+
+    }
+};
+
 #endif //ADS_HW2_HASHTABLE_H
---
+++ SinglyLinkedList.h
@@ -0,0 +1,206 @@
+//
+// Created by iansg on 1/17/2024.
+//
+
+#ifndef ADS_HW2_SINGLYLINKEDLIST_H
+#define ADS_HW2_SINGLYLINKEDLIST_H
+
+#include <stdexcept>
+using namespace std;
+
+template<typename T>
+class singly_linked_list {
+    class Node {
+        T value {};
+        Node* next {};
+        friend singly_linked_list;
+    public:
+        Node() = default;
+        explicit Node(T val) : value(val) {}
+    };
+
+    Node* head {};
+    int sz {};
+
+    void _reverse(Node* prev, Node* cur) {
+        prev->next = nullptr;
+
+        if (cur->next == nullptr) {
+            cur->next = prev;
+            head = cur;
+        }
+        else {
+            _reverse(cur, cur->next);
+            cur->next = prev;
+        }
+    }
+
+    void _insertion_sort(Node* cur, int c) {
+        if (c == 0) return;
+        _insertion_sort(cur->next, --c);
+        if (cur->value > cur->next->value)
+            swap(cur->value, cur->next->value);
+    }
+
+public:
+    singly_linked_list() = default;
+    T front() { return head->value; }
+    T back() {
+        Node* temp = head;
+        while (temp->next != nullptr) {
+            temp = temp->next;
+        }
+        return temp->value;
+    }
+    void push_front(T val) {
+        ++sz;
+
+        Node* node = new Node(val);
+        node->next = head;
+        head = node;
+    }
+    void push_back(T val) {
+        ++sz;
+
+        if (head == nullptr) {
+            head = new Node(val);
+            return;
+        }
+        Node* temp = head;
+        while (temp->next != nullptr) {
+            temp = temp->next;
+        }
+        Node* node = new Node(val);
+        temp->next = node;
+    }
+    T pop_front() {
+        if (head == nullptr)
+            throw runtime_error("Can't pop when list is empty.");
+        Node* temp = head;
+        head = temp->next;
+
+        --sz;
+
+        T el = temp->value;
+        delete temp;
+        return el;
+    }
+    T pop_back() {
+        if (head == nullptr)
+            throw runtime_error("Can't pop when list is empty.");
+
+        --sz;
+
+        if (head->next == nullptr) {
+            T el = head->value;
+            delete head;
+            head = nullptr;
+            return el;
+        }
+        Node* temp = head;
+        while (temp->next->next != nullptr) {
+            temp = temp->next;
+        }
+
+        T el = temp->next->value;
+        delete temp->next;
+        temp->next = nullptr;
+        return el;
+    }
+    T operator[](int pos) {
+        if (pos >= sz || pos < 0)
+            throw invalid_argument("Not a valid index.");
+        Node* temp = head;
+        for (int i = 0; i < pos; ++i)
+            temp = temp->next;
+        return temp->value;
+    }
+    bool empty() { return head == nullptr; }
+    int size() { return sz; }
+    void clear() {
+        if (head == nullptr) return;
+
+        Node* temp = head;
+        while (head->next != nullptr) {
+            temp = head;
+            head = temp->next;
+            delete temp;
+        }
+        delete head;
+        head = nullptr;
+    }
+    // insertion sort
+    void sort() {
+        for (int i = 0; i < sz; ++i)
+            _insertion_sort(head, i);
+    }
+    void reverse() {
+        if (head && head->next)
+            _reverse(head, head->next);
+    }
+    singly_linked_list<T> merge(const singly_linked_list<T>& other) {
+        int i {}, j {};
+        Node* n1 = head;
+        Node* n2 = other.head;
+        singly_linked_list<T> result;
+        while (i < sz && j < other.sz) {
+            if (n1->value > n2->value) {
+                result.push_back(n2->value);
+                n2 = n2->next;
+                ++j;
+            }
+            else  {
+                result.push_back(n1->value);
+                n1 = n1->next;
+                ++i;
+            }
+        }
+        while (i < sz) {
+            result.push_back(n1->value);
+            n1 = n1->next;
+            ++i;
+        }
+        while (j < other.sz) {
+            result.push_back(n2->value);
+            n2 = n2->next;
+            ++j;
+        }
+        return result;
+    }
+    singly_linked_list<T> intersect(const singly_linked_list& other) {
+        int i {}, j {};
+        Node* n1 = head;
+        Node* n2 = other.head;
+        singly_linked_list<T> result;
+        while (i < sz && j < other.sz) {
+            if (n1->value == n2->value) {
+                result.push_back(n1->value);
+                n1 = n1->next, n2 = n2->next;
+                ++i, ++j;
+            }
+            else  if (n1->value < n2->value) {
+                n1 = n1->next;
+                ++i;
+            }
+            else {
+                n2 = n2->next;
+                ++j;
+            }
+        }
+        return result;
+    }
+    friend ostream& operator<<(ostream& out, singly_linked_list sll) {
+        Node* temp = sll.head;
+        if (temp != nullptr) {
+            out << temp->value;
+            while (temp->next != nullptr) {
+                temp = temp->next;
+                out << ' ' << temp->value;
+            }
+        }
+        out << '\n';
+        return out;
+    }
+};
+
+#endif //ADS_HW2_SINGLYLINKEDLIST_H
